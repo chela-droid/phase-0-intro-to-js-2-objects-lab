57ffd37f256fd54f1e718227138118660f1c9da5
chela-droid <[200~chelangatcaroline70@gmail.com~>
2022-11-28 12:32:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 // Write your solution in this file!
+const employee = {
+    name:'caroline', 
+    streetAddress:'yaya'}
+    
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    const newEmployee = {...employee}
+     newEmployee[key]=value;
+      return newEmployee;
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key , value){
+        newEmployee[key]= value;
+         return employee;
+}
+
+const newEmployee = updateEmployeeWithKeyAndValue(
+    employee,
+    "tittle",
+    "assistant"
+);
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+//     const streetAddress={employee}
+//     newEmployee[key]=value
+//     return ('streetAddress'='12 Broadway')
+// }
+
+// function destructivelyUpdateEmployeeWithKeyAndValue (employeee, key, value) {
+    // employee[key]=value;
+    // return ('Sam');
+// }
+
+destructivelyUpdateEmployeeWithKeyAndValue(
+    employee,
+    "placeOfBirth",
+    "Yaya",
+);
+function deleteFromEmployeeByKey(employee, key) {
+    const newDetails={employee};
+    delete newDetails[key];
+    return newDetails;
+}
+deleteFromEmployeeByKey(
+    employee,
+    "placeOfBirth",
+);
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+destructivelyDeleteFromEmployeeByKey(
+    employee,
+    "streetName",
+);
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(){
+//     'name:Sam'
+//     'streetAddress:12 Broadway'
+// }
